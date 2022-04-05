2b15871038aba9488d276b7d137310d3057eee5d
5hdumatt <5hdumatt@gmail.com>
2022-04-05 21:54:28 +0900

    Project Start.
    반려동물의 사료 성분을 분석해주는 토이프로젝트 yum-yum

---
+++ src/test/java/toy/yumyum/YumYumApplicationTests.java
@@ -0,0 +1,13 @@
+package toy.yumyum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YumYumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
